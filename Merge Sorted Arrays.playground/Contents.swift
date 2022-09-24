cf0b448d75e2f530d99a1c75159ba5b1dc707779
Yusuf Keskin <yusuf_keskin@outlook.com>
2022-09-25 01:07:39 +0300

    new solutions added

--- Merge Sorted Arrays.playground/Contents.swift
+++ Merge Sorted Arrays.playground/Contents.swift
@@ -5,27 +5,36 @@ var nums1 = [1,2,3,0,0,0]
 var nums2 = [2,5,6]
 
 func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
-
-    var i = m - 1
-    var j = n - 1
-    var k = m + n - 1
     
-    while nums1.last == 0 {
-        nums1.removeLast()
+        var firstIndex = m - 1
+        var secondIndex = n - 1
+        var last = nums1.count - 1
+        while firstIndex >= 0 || secondIndex >= 0 {
+            defer {
+                last -= 1
+            }
+            
+            if secondIndex < 0 {
+                nums1[last] = nums1[firstIndex]
+                firstIndex -= 1
+                continue
+            }
+            
+            if firstIndex < 0 {
+                nums1[last] = nums2[secondIndex]
+                secondIndex -= 1
+                continue
+            }
+            
+            if nums2[secondIndex] >= nums1[firstIndex] {
+                nums1[last] = nums2[secondIndex]
+                secondIndex -= 1
+                continue
+            }
+            
+            nums1[last] = nums1[firstIndex]
+            firstIndex -= 1
     }
-    
-    while i >= 0 && j >= 0 {
-        if nums1[i] > nums2[j] {
-            nums1[k] = nums1[i]
-            k -= 1
-            i -= 1
-        } else {
-            nums1[k] = nums2[j]
-            k -= 1
-            j -= 1
-        }
-    }
-
 }
 
-merge(&nums1, 6 , nums2, 3)
+merge(&nums1, 3 , nums2, 3)
